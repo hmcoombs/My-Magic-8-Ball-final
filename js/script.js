3b6573e0df8de350ff4907452191f3b703e50092
smackmhard <104869058+smackmhard@users.noreply.github.com>
2022-10-27 21:17:43 -0400

    chg ccs path

--- js/script.js
+++ js/script.js
@@ -2,12 +2,9 @@ let button = document.getElementById("button");
 let input = document.getElementById("input");
 let answer = document.getElementById("answer");
 let eight = document.getElementById("eight");
-let options = ["It is certain","Are you MAD","It is decidedly so","Without a doubt","Yes – definitely","Stay this Madness",
-"You may rely on it","As I see it, yes","Most likely","Outlook good","Yes","Signs point to yes",
+let options = ["It is certain","Are you MAD","It is decidedly so","Without a doubt","Yes – definitely","Stay this Madness","You may rely on it","As I see it, yes","Most likely","Outlook good","Yes","Signs point to yes",
 "Don’t count on it","My reply is no","The cosmos says no","Outlook not so good","Very doubtful",
 "Reply hazy, try again","Ask again later","Better not tell you now","Cannot predict now","Concentrate and ask again","The stars are not aligning"];
-
-
 
 
 button.addEventListener("click", function() {
@@ -18,25 +15,24 @@ button.addEventListener("click", function() {
          else {
             eight.innerHTML = '';
             answer.innerHTML = 'Searching the Cosmos, stand by!'
-            setTimeout(hold,  3000);
-            
+            setTimeout(hold,  3000);            
           }
 
-                 
         function hold(){
         eight.innerHTML = '';         
         let num = Math.floor(Math.random()* options.length);       
         answer.innerHTML = options[num];       
         setTimeout(timeUp, 3000);
+
         }
          
          function timeUp(){
           document.getElementById("input").value='';
           document.getElementById("eight").innerHTML='8';
           document.getElementById("answer").innerHTML='';
-            }
+          }
               
-         }
+        }
         
       );
     
